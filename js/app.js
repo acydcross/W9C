60563990511f346192bc9eee1e524734793483b4
acydcross <acydcross@hotmail.com>
2020-08-20 22:18:13 -0600

    Created an array JS Objects

---
+++ js/app.js
@@ -0,0 +1,65 @@
+var tweets = [
+    {
+        "content" : "Sleep is the perfect escape from everything.",
+        "username" : "@wordstionary",
+        "created at" : "08/03/20",
+    },
+
+    {
+        "content" : "Don't be ashamed to fart while you urinate... There is no rain without thunder.",
+        "username" : "@memes",
+        "created at" : "08/18/20",
+    },
+
+    {
+        "content" : "Enter password, wrong, wrong, wrong, reset password, new password can't be old password. -_-",
+        "username" : "@thatkiddmigs",
+        "created at" : "08/16/20",
+    },
+
+    {
+        "content" : "I wish we had the ability to dream together with other people. Like multi-player dreaming.",
+        "username" : "@TheWeirdWorld",
+        "created at" : "12/18/15",
+    },
+
+    {
+        "content" : "In 47 years, Joe did none of the things of which he now speaks. He will never change, just words!",
+        "username" : "@realDonaldTrump",
+        "created at" : "08/20/20",
+    },
+
+    {
+        "content" : "So many memories Red heart Thanks for celebrating with us! Directioners forever #10YearsOf1D",
+        "username" : "@onedirection",
+        "created at" : "07/29/20",
+    },
+
+    {
+        "content" : "Neither love nor terror makes one blind: indifference makes one blind.” — James Baldwin. We stand with our Black colleagues, employees, fans, actors, storytellers — and all affected by senseless violence. #BlackLivesMatter",
+        "username" : "@realDonaldTrump",
+        "created at" : "05/30/20",
+    },
+
+    {
+        "content" : "Leadership.",
+        "username" : "@lionel_anderson",
+        "created at" : "05/30/20",
+    },
+
+    {
+        "content" : "This November, Wolverine's bloodiest adventure begins.",
+        "username" : "@Marvel",
+        "created at" : "08/17/20",
+    },
+
+    {
+        "content" : "Your choices may lead you down paths of light and darkness. True or False: Players can choose to follow more than one religion at a time.",
+        "username" : "@AshesofCreation",
+        "created at" : "08/18/20",
+    },
+];
+
+for(var index=0; index < tweets.length; index++) {
+    console.log(Object.values(tweets[index]));
+}
